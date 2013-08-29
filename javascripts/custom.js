bb02edc8ad10dc601797fe0433806ebc16351e61
Michael Wilson <mwilson@convercent.com>
2013-08-29 09:58:13 -0600

    need to complete the scrollTo functonality

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,4 +1,15 @@
-function scrollToTop() {
-	var top = $(p.title).scrollTop();
-	console.log("top = " + top);
+function scrollToTop(el) {
+	var nTop = $(el).offset().top; //$(el).scrollTop();
+
+	if (nTop > 0) {
+		$('ul.data-list').scrollTo(nTop, 500);
+	}
+
+	/*
+	$('ul.data-list').animate({
+		scrollTop: 0
+		}, 1000);
+
+	*/
+	console.log("top = " + nTop);
 }
